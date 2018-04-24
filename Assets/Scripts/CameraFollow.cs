e03453947bfbf21c48e6da1a8f63ed8d49389c45
DANIEL-FG\o9708 <o97082@yahoo.com.tw>
2018-04-24 14:36:33 +0800

    Added CameraFollow.cs and arena setup

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private GameObject player;
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    void LateUpdate ()
+    {
+        transform.position = player.transform.position;
+	}
+}
