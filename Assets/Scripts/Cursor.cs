8ba03b35c3a2a95145273beeb37d540093830374
DANIEL-FG\o9708 <o97082@yahoo.com.tw>
2018-04-25 23:49:13 +0800

    Added CameraRaycaster.cs

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    private CameraRaycaster cameraRaycaster;
+	// Use this for initialization
+	void Start ()
+    {
+        cameraRaycaster = FindObjectOfType<CameraRaycaster>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log(cameraRaycaster.LayerHit);
+	}
+}
