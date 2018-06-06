afcb3674192d22b40a3498e0f55f92240afb0b6c
DANIEL-FG\o9708 <o97082@yahoo.com.tw>
2018-06-06 22:41:04 +0800

    Added pathfinding behavior to enemy and changed enemy prefab

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private float maxHealth = 100f;
+    [SerializeField] private float maxHealth = 100f;  //最大血量
+    [SerializeField] private float detectRadius = 5f;  //玩家進到此偵測範圍內會追逐玩家
 
+    private AICharacterControl aiCharacterControl;
+    private GameObject player;
+    private ThirdPersonCharacter thirdPersonControl = null;
     private float currentHealth = 100f;
-
     public float HealthAsPercentage
     {
         get
         {
             return currentHealth / maxHealth;
+        }
+    }
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        aiCharacterControl = GetComponent<AICharacterControl>();
+        thirdPersonControl = GetComponent<ThirdPersonCharacter>();
+    }
+
+    private void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        if(distanceToPlayer <= detectRadius)
+        {
+            aiCharacterControl.SetTarget(player.transform);
+        }
+        else
+        {
+            aiCharacterControl.SetTarget(transform);
         }
     }
 }
